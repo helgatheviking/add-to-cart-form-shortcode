02edd812d28acac06fa129fd590303da2be76f0d
Kathy Darling <helgatheviking@gmail.com>
2017-12-14 17:35:53 +0100

    initial commit

---
+++ wc-add-to-cart-form-shortcode.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Plugin Name: Add to Cart Form Shortcode for WooCommerce
+ * Plugin URI: https://woocommerce.com/
+ * Description: Add [add_to_cart_form] shortcode that display a single product add to cart form.
+ * Version: 1.0.0
+ * Author: helgatheviking
+ * Author URI: https://kathyisawesome.com
+ * Requires at least: 4.8
+ * Tested up to: 4.9
+ * WC requires at least: 3.2.0
+ * WC tested up to: 3.2.6
+ *
+ * @package Add to Cart Form Shortcode
+ * @category Core
+ * @author helgatheviking
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+
+/**
+ * Add [add_to_cart_form] shortcode that display a single product add to cart form
+ * Supports id and sku attributes [add_to_cart_form id=99] or [add_to_cart_form sku=123ABC]
+ * Essentially a duplicate of the [product_page]
+ * but replacing wc_get_template_part( 'content', 'single-product' ); with woocommerce_template_single_add_to_cart()
+ *
+ * @param array $atts Attributes.
+ * @return string
+ */
+function kia_add_to_cart_form_shortcode( $atts ) {
+		if ( empty( $atts ) ) {
+			return '';
+		}
+
+		if ( ! isset( $atts['id'] ) && ! isset( $atts['sku'] ) ) {
+			return '';
+		}
+
+		$args = array(
+			'posts_per_page'      => 1,
+			'post_type'           => 'product',
+			'post_status'         => 'publish',
+			'ignore_sticky_posts' => 1,
+			'no_found_rows'       => 1,
+		);
+
+		if ( isset( $atts['sku'] ) ) {
+			$args['meta_query'][] = array(
+				'key'     => '_sku',
+				'value'   => sanitize_text_field( $atts['sku'] ),
+				'compare' => '=',
+			);
+
+			$args['post_type'] = array( 'product', 'product_variation' );
+		}
+
+		if ( isset( $atts['id'] ) ) {
+			$args['p'] = absint( $atts['id'] );
+		}
+
+		$single_product = new WP_Query( $args );
+
+		$preselected_id = '0';
+
+		// Check if sku is a variation.
+		if ( isset( $atts['sku'] ) && $single_product->have_posts() && 'product_variation' === $single_product->post->post_type ) {
+
+			$variation = new WC_Product_Variation( $single_product->post->ID );
+			$attributes = $variation->get_attributes();
+
+			// Set preselected id to be used by JS to provide context.
+			$preselected_id = $single_product->post->ID;
+
+			// Get the parent product object.
+			$args = array(
+				'posts_per_page'      => 1,
+				'post_type'           => 'product',
+				'post_status'         => 'publish',
+				'ignore_sticky_posts' => 1,
+				'no_found_rows'       => 1,
+				'p'                   => $single_product->post->post_parent,
+			);
+
+			$single_product = new WP_Query( $args );
+		?>
+			<script type="text/javascript">
+				jQuery( document ).ready( function( $ ) {
+					var $variations_form = $( '[data-product-page-preselected-id="<?php echo esc_attr( $preselected_id ); ?>"]' ).find( 'form.variations_form' );
+
+					<?php foreach ( $attributes as $attr => $value ) { ?>
+						$variations_form.find( 'select[name="<?php echo esc_attr( $attr ); ?>"]' ).val( '<?php echo esc_js( $value ); ?>' );
+					<?php } ?>
+				});
+			</script>
+		<?php
+		}
+
+		// For "is_single" to always make load comments_template() for reviews.
+		$single_product->is_single = true;
+
+		ob_start();
+
+		global $wp_query;
+
+		// Backup query object so following loops think this is a product page.
+		$previous_wp_query = $wp_query;
+		// @codingStandardsIgnoreStart
+		$wp_query          = $single_product;
+		// @codingStandardsIgnoreEnd
+
+		wp_enqueue_script( 'wc-single-product' );
+
+		while ( $single_product->have_posts() ) {
+			$single_product->the_post()
+			?>
+			<div class="single-product" data-product-page-preselected-id="<?php echo esc_attr( $preselected_id ); ?>">
+				<?php woocommerce_template_single_add_to_cart(); ?>
+			</div>
+			<?php
+		}
+
+		// Restore $previous_wp_query and reset post data.
+		// @codingStandardsIgnoreStart
+		$wp_query = $previous_wp_query;
+		// @codingStandardsIgnoreEnd
+		wp_reset_postdata();
+
+		return '<div class="woocommerce">' . ob_get_clean() . '</div>';
+}
+
+add_shortcode( 'add_to_cart_form', 'kia_add_to_cart_form_shortcode' );
